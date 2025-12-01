0e119fb4738c36397cb709a07669cdc0be0b2aa0
adyvka31 <rafifdyvka07@gmail.com>
2025-12-01 16:50:23 +0700

    build: add firebase into flutter project

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:ecommerce_mobile/features/splash/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:ecommerce_mobile/prefrences/color.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
